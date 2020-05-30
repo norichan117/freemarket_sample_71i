90497b508312e4cc3fe9fa425824458a29a30dbf
kesaki <64cr12@gmail.com>
2020-05-30 21:27:24 +0900

    devise model 作成後

--- db/migrate/20200530042032_devise_create_users.rb
+++ db/migrate/20200530042032_devise_create_users.rb
@@ -3,14 +3,14 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :nickname            null: false
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false
       t.string :first_name,         null: false
-      t.string :family_name_kana    null: false
-      t.string :first_name_kana     null: false
-      t.date   :birthday            null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false
+      t.date   :birthday,           null: false
 
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
