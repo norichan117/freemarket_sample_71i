bf5a7e758705ca68c65c9d5f025988f31643542d
tomoki1119 <kanon.dmc@gmail.com>
2020-05-31 17:25:00 +0900

    修正２

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'mypages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "mypages#index"
-
+  root "items#index"
 end
