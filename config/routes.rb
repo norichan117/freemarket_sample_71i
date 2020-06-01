51d48342647d02767a2bf1240387ff055444bedc
kesaki <64cr12@gmail.com>
2020-06-01 21:12:27 +0900

    サインアップ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
-
-  get 'users/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "items#index"
+  resources :users, only: [:index, :edit]
+  resources :items, only: [:index]
 end
