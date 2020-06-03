992113d6eeac9cfafdf24d7453fdcbe552db9106
kumati7 <kumati1208@gmail.com>
2020-06-03 19:30:34 +0900

    商品詳細

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'items/index'
-
   get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
+  # devise_for :users
+  root "items#index"
+  resources :users, only: [:index, :edit]
+  resources :items, only: [:index]
 end
