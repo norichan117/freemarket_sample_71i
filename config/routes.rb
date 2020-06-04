fa881b6dc43b567a89e1f651bbf2739d64535b51
kumati7 <kumati1208@gmail.com>
2020-06-04 16:20:57 +0900

    商品詳細2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :users, only: [:index, :edit]
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index,]
 end
