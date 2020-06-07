dd8bd213c63f23996652c4734f23f6eb7c15b814
kesaki <64cr12@gmail.com>
2020-06-07 22:32:00 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20200604132342) do
+ActiveRecord::Schema.define(version: 20200606121546) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "category_name"
@@ -33,8 +32,14 @@ ActiveRecord::Schema.define(version: 20200604132342) do
     t.integer  "trading_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
 
-
+  create_table "user_cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",     null: false
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
