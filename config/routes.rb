1d839dc2b2e9991c738c78c68e7711dd12d33e1e
kesaki <64cr12@gmail.com>
2020-06-08 21:34:43 +0900

    devise コントローラ作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
   root "items#index"
   resources :users, only: [:index, :edit]
   resources :items, only: [:index, :show]
