0fafdae5583e4e4ef390644bf5cceff3155c022c
kumati7 <kumati1208@gmail.com>
2020-06-10 20:58:07 +0900

    商品詳細11

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,2 +1,3 @@
 class CategorysController < ApplicationController
+  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,3 @@
 class Category < ApplicationRecord
   has_many :items
-  has_many :images
-  belongs_to :category
-  belongs_to :brand
 end
