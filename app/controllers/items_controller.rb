8d15410ab05f145c422b176603aadb83650d45b7
kumati7 <kumati1208@gmail.com>
2020-06-11 14:57:03 +0900

    商品詳細リンク付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,7 @@ class ItemsController < ApplicationController
   def show
     # @item = Item.find(params[:id])
     @item = Item.find(1)
+    @user = User.find(1)
+    @image = Image.find(1)
   end
 end
