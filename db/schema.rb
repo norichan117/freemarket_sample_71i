229255a4d2d42d4282a6ac64804ddbb570acfceb
kesaki <64cr12@gmail.com>
2020-06-13 16:27:46 +0900

    送り先情報画面、ビュー統合

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20200612154338) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "item_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.string   "item_name"
