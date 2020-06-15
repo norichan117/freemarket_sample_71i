59703752fb3c15267f1193d5d126a0ed43c57f22
kesaki <64cr12@gmail.com>
2020-06-15 20:13:54 +0900

    本人情報登録、お届け先情報登録画面をサイドメニューと統合

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,12 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end 
+
+  def edit
+  end
   # GET /resource/sign_in
   # def new
   #   super
