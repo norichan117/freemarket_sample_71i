f3ee89872159a8dd548f2ed14e91bb83a701e3cb
kesaki <64cr12@gmail.com>
2020-06-16 20:00:12 +0900

    edit itmes show  cont roller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+
+  def index
+    abc = Category.find(1)
+    @categories = abc.subtree
+    binding.pry
+  end
+
   def show
     # @item = Item.find(params[:id])
     @item = Item.find(1)
