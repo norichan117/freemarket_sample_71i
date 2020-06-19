631692b1c36ec99bd2664a46e4d751e883dd20b5
kesaki <64cr12@gmail.com>
2020-06-20 02:00:25 +0900

    header試行

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,8 +1,8 @@
 class CategorysController < ApplicationController
   def index
-    @parents = Category.all.order("id ASC").limit(13)
-    @category = Category.all
+    @parents = Category.where(ancestry: nil)
     @brand = Brand.all
+
   end
 
   def show
