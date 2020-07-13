022e546863cfb3bf4d0f4ec9fc46b769b26242ca
norichan117 <adgjm2355@gmail.com>
2020-07-13 12:02:20 +0900

    画像保存先の条件分岐

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,4 +19,19 @@ CarrierWave.configure do |config|
     config.enable_processing = false if Rails.env.test?
   end
 
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], #credentails.ymlに鍵の本体があります
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  #credentails.ymlに鍵の本体があります
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'gasiwatani'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gasiwatani'
+  end
+
 end
