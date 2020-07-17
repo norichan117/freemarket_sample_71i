bccb97a9bdf22e197464b415560e600b7ba93bfc
kumati7 <kumati1208@gmail.com>
2020-07-17 20:15:56 +0900

    パンくず3

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,38 +2,56 @@ crumb :root do
   link "トップページ", root_path
 end
 
-crumb :category_index do
-  link "カテゴリー一覧", categorys_path
+crumb :child_category do
+  link "#{Category.find(params[:id]).category_name}", category_path
+  parent :root
 end
 
-# 親カテゴリーのパンくず
-crumb :parent_category do |category|
-  category = Category.find(params[:id]).root
-  link "#{category.name}", category_path(category)
-  parent :category_index
-end
-# -----------------------------------------------------------------
-# 子カテゴリーのパンくず
-crumb :child_category do |category|
-  category = Category.find(params[:id])
-  # 表示しているページが子カテゴリーの一覧ページの場合
-  if category.has_children?
-    link "#{category.name}", category_path(category)
-    parent :parent_category
-
-  # 表示しているページが孫カテゴリーの一覧ページの場合
-  else
-    link "#{category.parent.name}", category_path(category.parent)
-    parent :parent_category
-  end
-end
-# -----------------------------------------------------------------
-# 孫カテゴリーのパンくず
-crumb :grandchild_category do |category|
-  category = Category.find(params[:id])
-  link "#{category.name}", category_path(category)
+crumb :grandchild_category do
+  link "#{Category.find(params[:id]).category_name}"
   parent :child_category
 end
+
+
+# crumb :root do
+#   link "トップページ", root_path
+# end
+
+# crumb :category_index do
+#   link "カテゴリー一覧", categorys_path
+# end
+
+# # 親カテゴリーのパンくず
+# crumb :parent_category do |category|
+#   category = Category.find(params[:id]).root
+#   link "#{category.name}", category_path(category)
+#   parent :category_index
+# end
+# # -----------------------------------------------------------------
+# # 子カテゴリーのパンくず
+# crumb :child_category do |category|
+#   category = Category.find(params[:id])
+#   # 表示しているページが子カテゴリーの一覧ページの場合
+#   if category.has_children?
+#     link "#{category.name}", category_path(category)
+#     parent :parent_category
+
+#   # 表示しているページが孫カテゴリーの一覧ページの場合
+#   else
+#     link "#{category.parent.name}", category_path(category.parent)
+#     parent :parent_category
+#   end
+# end
+# # -----------------------------------------------------------------
+# # 孫カテゴリーのパンくず
+# crumb :grandchild_category do |category|
+#   category = Category.find(params[:id])
+#   link "#{category.name}", category_path(category)
+#   parent :child_category
+# end
+
+
+
 
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
