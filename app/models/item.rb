b92ede94e789a6874d5f5fe2b423918b8e09e498
kumati7 <kumati1208@gmail.com>
2020-07-25 16:33:41 +0900

    item_newバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,11 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   has_many :favorites, dependent: :destroy
-
+  
+  validates :category, presence: true
+  validates :condition, presence: true
+  validates :postage_burden, presence: true
+  validates :shipping_area, presence: true
+  validates :days_to_ship, presence: true
+  validates :price, presence: true
 end
