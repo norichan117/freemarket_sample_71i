5d71bdef1dddcd50181003fe1de7587aafd20808
kesaki <64cr12@gmail.com>
2020-07-28 21:41:06 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,17 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-  #   config.storage = :fog
-  #   config.fog_provider = 'fog/aws'
-  #   config.fog_directory  = 'gasiwatani'
-  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gasiwatani'
-  #   config.fog_credentials = {
-  #     provider: 'AWS',
-  #     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-  #     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-  #     region: 'ap-northeast-1'
-  #   }
-  # else 
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'gasiwatani'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gasiwatani'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+  else 
     config.storage = :file
     config.enable_processing = false if Rails.env.test?
   end
