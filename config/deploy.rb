9a8b79c2d70dcb6af9e262421dfdf39582cf02c4
norichan117 <62635452+norichan117@users.noreply.github.com>
2021-03-12 19:03:19 +0900

    今日

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.14.0'
 set :application, 'freemarket_sample_71i'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kesaki/freemarket_sample_71i.git'
+set :repo_url,  'git@github.com:norichan117/freemarket_sample_71i.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
