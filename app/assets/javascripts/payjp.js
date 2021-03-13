13ad16ed6adabbd6d7b28be54b3aa79d6d53857e
norichan117 <62635452+norichan117@users.noreply.github.com>
2021-03-13 21:53:49 +0900

    2021/03/13

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,17 +1,23 @@
-$(document).on('turbolinks:load', ()=> {
+window.addEventListener('DOMContentLoaded', function(){
 
-  $('#payment_card_submit-button').click(function(e){ 
+  let submit = document.getElementById("payment_card_submit-button");
+
+  Payjp.setPublicKey('pk_test_9f8e819279c2a12de90f9455');
+
+    submit.addEventListener('click', function(e){ 
+
     e.preventDefault(); 
-    Payjp.setPublicKey('pk_test_9f8e819279c2a12de90f9455');
+
     let card = { 
-        number: $("#payment_card_no").val(),
-        cvc: $("#payment_card_cvc").val(),
-        exp_month: $("#payment_card_month").val(),
-        exp_year: $("#payment_card_year").val()
+        number: document.getElementById("payment_card_no").value,
+        cvc: document.getElementById("payment_card_cvc").value,
+        exp_month: document.getElementById("payment_card_month").value,
+        exp_year: document.getElementById("payment_card_year").value
     };
 
     Payjp.createToken(card, function(status, response) {  
       if (status === 200) { 
+        
         $(".number").removeAttr("name");
         $(".cvc").removeAttr("name");
         $(".exp_month").removeAttr("name");
