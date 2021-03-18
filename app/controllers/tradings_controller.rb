c3039b77460224190a508dd2a0a0e85edc9aa83e
norichan117 <62635452+norichan117@users.noreply.github.com>
2021-03-18 17:47:18 +0900

    Update tradings_controller.rb

--- app/controllers/tradings_controller.rb
+++ app/controllers/tradings_controller.rb
@@ -1,31 +1,40 @@
 class TradingsController < ApplicationController
+  before_action :authenticate_user!
   layout 'simple'
   require "payjp"
   def new
     @item = Item.find(params[:item_id])
     @deliver_address = current_user.deliver_address
+    unless @deliver_address.present?
+      redirect_to new_deliver_address_path
+    else
     @user_card = current_user.user_card
     @card = UserCard.find_by(user_id: current_user.id)
-    Payjp.api_key = ('sk_test_aaea1ea08c2f1efc81505081')
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @customer_card = customer.cards.retrieve(@card.card_id)
-      @card_brand = @customer_card.brand
-      case @card_brand
-      when "Visa"
-        @card_src = "visa.png"
-      when "JCB"
-        @card_src = "jcb.png"
-      when "MasterCard"
-        @card_src = "master.png"
-      when "American Express"
-        @card_src = "amex.png"
-      when "Diners Club"
-        @card_src = "diners.png"
-      when "Discover"
-        @card_src = "discover.png"
-      end
-      @exp_month = @customer_card.exp_month.to_s
-      @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+    if  @card.blank?
+        redirect_to new_user_card_path
+    else
+      Payjp.api_key = ('sk_test_aaea1ea08c2f1efc81505081')
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        @customer_card = customer.cards.retrieve(@card.card_id)
+        @card_brand = @customer_card.brand
+        case @card_brand
+        when "Visa"
+          @card_src = "visa.png"
+        when "JCB"
+          @card_src = "jcb.png"
+        when "MasterCard"
+          @card_src = "master.png"
+        when "American Express"
+          @card_src = "amex.png"
+        when "Diners Club"
+          @card_src = "diners.png"
+        when "Discover"
+          @card_src = "discover.png"
+        end
+        @exp_month = @customer_card.exp_month.to_s
+        @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+    end
+  end
   end
 
   def create
